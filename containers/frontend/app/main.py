154eeb71aa045071165a6b58a07f7f99e0de24b0
cikeZ00 <17727672+cikeZ00@users.noreply.github.com>
2025-02-09 07:31:19 +0100

    Strip down the code

--- containers/frontend/app/main.py
+++ containers/frontend/app/main.py
@@ -3,278 +3,129 @@ import json
 import requests
 import uvicorn
 import base64
-import boto3
-import hurry.filesize
 from fastapi import FastAPI, HTTPException, Response, File, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from cryptography.fernet import Fernet
 
 PRIVATE_KEY = os.environ.get("PRIVATE_KEY")
-BUCKET_REGION = os.environ.get("BUCKET_REGION")
-SCW_ACCESS_KEY = os.environ.get("SCW_ACCESS_KEY")
-SCW_SECRET_KEY = os.environ.get("SCW_SECRET_KEY")
-IP_STATE_BUCKET_NAME = os.environ.get("IP_STATE_BUCKET_NAME")
-REPLAY_FILES_BUCKET_NAME = os.environ.get("REPLAY_FILES_BUCKET_NAME")
-FILES_FOR_DOWNLOAD_BUCKET_NAME = os.environ.get("FILES_FOR_DOWNLOAD_BUCKET_NAME")
+SERVER = "https://tv.vankrupt.net"
+DATA_DIR = "data"
 
-SERVER = "http://tv.pavlov-vr.com"
+HEADERS = {
+    "Host": "tv.vankrupt.net",
+    "Accept": "*/*",
+    "User-Agent": "Pavlov/++UE5+Release-5.1-CL-23901901 Windows/10.0.22631.1.256.64bit"
+}
+
+os.makedirs(DATA_DIR, exist_ok=True)
 
 app = FastAPI(
-    title="tv.pavlovhosting.com",
-    description="Download and replay Pavlov TV files\n"
-                "1. Add the following to your hosts file: "
-                "51.15.238.121 tv.pavlov-vr.com\n"
-                "2. Check you can reach this page: \n"
-                "http://tv.pavlov-vr.com/__tv.pavlovhosting.com/relay\n"
-                "3. Start downloading and uploading replays!",
+    title="LocalPavTV",
+    description="Download and replay Pavlov TV files",
     version="0.0.1",
-    contact={
-        "name": "PavlovHosting.com"
-    }
 )
-
-session = boto3.Session(region_name=BUCKET_REGION)
-resource = session.resource(
-    's3',
-    endpoint_url=f"https://s3.{BUCKET_REGION}.scw.cloud",
-    aws_access_key_id=SCW_ACCESS_KEY,
-    aws_secret_access_key=SCW_SECRET_KEY
-)
-
 
 @app.get("/")
 def serve_homepage():
     return RedirectResponse("/docs")
 
-
 def bytes_to_base64(bytes_str):
     return base64.b64encode(bytes_str).decode("ascii")
 
-
 def base64_to_bytes(base64_str):
     return base64.b64decode(base64_str)
-
 
 @app.get("/list")
 def list_interesting_games():
     games_list = requests.get(SERVER + "/find/any?dummy=0")
     games_list.raise_for_status()
     games = games_list.json()
-    interesting_games = []
-    for replay in games["replays"]:
-        if len(replay["users"]) > 0 and replay["live"] is False:
-            interesting_games.append(replay)
-    return interesting_games
-
+    return [replay for replay in games["replays"] if replay["users"] and not replay["live"]]
 
 @app.get("/download/{replay_id}")
 def download_replay(replay_id: str):
-    # To download a replay we need to collect
-    # the /meta page
-    # the /event page
-    # the /startDownload page
-    # the steam files (replay.header and stream.1-2-3)
     if not replay_id.isalnum():
         raise HTTPException(status_code=404)
 
     replay_data = {}
     replay_files = {}
-
-    findAll = requests.get(
-        f"{SERVER}/find/any?dummy=0"
-    )
+    
+    findAll = requests.get(f"{SERVER}/find/any?dummy=0", verify=False, headers=HEADERS)
     findAll.raise_for_status()
     findAll_json = findAll.json()
-    findAllResponse = None
-    for playback in findAll_json["replays"]:
-        if playback["_id"] == replay_id:
-            findAllResponse = playback
+    findAllResponse = next((playback for playback in findAll_json["replays"] if playback["_id"] == replay_id), None)
 
-    if findAllResponse is None:
-        raise HTTPException(
-            status_code=400,
-            detail="A recording must still be available to download it."
-        )
+    if not findAllResponse:
+        raise HTTPException(status_code=400, detail="Recording not available.")
 
     replay_data["find"] = findAllResponse
 
     startDownload = requests.post(
-        f"{SERVER}/replay/{replay_id}/startDownloading?user"
+        f"{SERVER}/replay/{replay_id}/startDownloading?user", 
+        verify=False, 
+        headers=HEADERS
     )
     startDownload.raise_for_status()
     startDownload_json = startDownload.json()
+    
     if startDownload_json["state"] != "Recorded":
-        raise HTTPException(
-            status_code=400,
-            detail="A recording must be finished "
-                   "(not live) before it can be downloaded."
-        )
+        raise HTTPException(status_code=400, detail="Recording must be finished before download.")
+    
     replay_data["start_downloading"] = startDownload_json
 
-    meta = requests.get(f"{SERVER}/meta/{replay_id}")
+    meta = requests.get(f"{SERVER}/meta/{replay_id}", verify=False, headers=HEADERS)
     meta.raise_for_status()
     replay_data["meta"] = meta.json()
-
-    events = requests.get(f"{SERVER}/replay/{replay_id}/event")
+    
+    events = requests.get(f"{SERVER}/replay/{replay_id}/event", verify=False, headers=HEADERS)
     events.raise_for_status()
     replay_data["events"] = events.json()
+    
+    replay_dir = os.path.join(DATA_DIR, replay_id)
+    os.makedirs(replay_dir, exist_ok=True)
+    
+    with open(os.path.join(replay_dir, "replay.header"), "wb") as f:
+        f.write(requests.get(f"{SERVER}/replay/{replay_id}/file/replay.header", headers=HEADERS).content)
+    
+    for i in range(startDownload_json["numChunks"]):
+        with open(os.path.join(replay_dir, f"stream.{i}"), "wb") as f:
+            f.write(requests.get(f"{SERVER}/replay/{replay_id}/file/stream.{i}", headers=HEADERS).content)
+    
+    with open(os.path.join(replay_dir, "metadata.json"), "w") as f:
+        json.dump(replay_data, f)
 
-    # Now just download the stream files
-    header_response = requests.get(
-        f"{SERVER}/replay/{replay_id}/file/replay.header"
-    )
-    header_response.raise_for_status()
-    replay_files["replay.header"] = bytes_to_base64(header_response.content)
-    replay_files["replay.header.headers"] = json.dumps({})
-
-    for i in range(0, startDownload_json["numChunks"]):
-        file_response = requests.get(
-            f"{SERVER}/replay/{replay_id}/file/stream." + str(i)
-        )
-        file_response.raise_for_status()
-        replay_files["stream." + str(i)] = \
-            bytes_to_base64(file_response.content)
-        headers_json = json.dumps({
-            "MTime1": file_response.headers["MTime1"],
-            "MTime2": file_response.headers["MTime2"],
-            "NumChunks": file_response.headers["NumChunks"],
-            "State": file_response.headers["State"],
-            "Time": file_response.headers["Time"],
-            "Transfer-Encoding": file_response.headers["Transfer-Encoding"]
-        })
-        replay_files["stream." + str(i) + ".headers"] = bytes_to_base64(
-            headers_json.encode("utf-8")
-        )
-
-    full_content = {
-        "data": replay_data,
-        "files": replay_files
-    }
-
-    serialized_content = json.dumps(full_content)
-
-    # Encrypt file
-    fernet = Fernet(PRIVATE_KEY)
-    encrypted_content = fernet.encrypt(str.encode(serialized_content))
-
-    # Serve file
-    gamemode = meta.json()["gameMode"]
-    replayMap = findAllResponse["friendlyName"].strip()
-
-    return Response(
-        content=str.encode("1 tv.pavlovhosting.com\n") + encrypted_content,
-        media_type="application/pavlovtv",
-        headers={
-            "content-disposition":
-                "attachment; "
-                f"filename=\"{gamemode}-{replayMap}-{replay_id}.pavlovtv\""
-        }
-    )
-
+    return {"message": "Download completed", "path": replay_dir}
 
 @app.post("/upload")
 def upload(request: Request, file: bytes = File(...)):
-    # Decrypt the file
     fernet = Fernet(PRIVATE_KEY)
-    raw_file = file.decode()
-    file = str.encode("\n".join(raw_file.split("\n")[1:]))
     decrypted_content = fernet.decrypt(file)
-
-    # With the decrypted bytes, load the JSON file
     json_payload = json.loads(decrypted_content.decode())
-
+    
     replay_id = json_payload["data"]["find"]["_id"]
+    replay_dir = os.path.join(DATA_DIR, replay_id)
+    os.makedirs(replay_dir, exist_ok=True)
 
-    # Upload the replay files
-    replay_files_bucket = resource.Bucket(REPLAY_FILES_BUCKET_NAME)
-    for key in json_payload["files"]:
-        value = base64_to_bytes(json_payload["files"][key])
-        replay_files_bucket.put_object(
-            Body=value,
-            Key=replay_id + "/" + key
-        )
-
-    # Attach the data to the IP
-    ip_address = request.client.host
-
-    ip_state_bucket = resource.Bucket(IP_STATE_BUCKET_NAME)
-    ip_state_bucket.put_object(
-        Body=json.dumps({
-            "mounted_replay": json_payload["data"]
-        }),
-        Key=ip_address + ".json"
-    )
-
-    return {
-        "ok": True
-    }
-
+    for key, value in json_payload["files"].items():
+        with open(os.path.join(replay_dir, key), "wb") as f:
+            f.write(base64_to_bytes(value))
+    
+    with open(os.path.join(replay_dir, "metadata.json"), "w") as f:
+        json.dump(json_payload["data"], f)
+    
+    return {"ok": True}
 
 @app.post("/reset")
 def reset(request: Request):
     ip_address = request.client.host
-
-    resource.Bucket(IP_STATE_BUCKET_NAME).delete_objects(
-        Delete={
-            "Objects": [
-                {
-                    "Key": ip_address + ".json"
-                }
-            ]
-        }
-    )
-
-    return {
-        "ok": True
-    }
-
+    ip_file = os.path.join(DATA_DIR, f"{ip_address}.json")
+    if os.path.exists(ip_file):
+        os.remove(ip_file)
+    return {"ok": True}
 
 @app.get("/whoami")
 def whoami(request: Request):
-    return {
-        "ip": request.client.host
-    }
-
-
-@app.get("/all_recordings")
-def all_recordings_html():
-    anchor_list = ""
-    entries = {}
-    for s3_object in resource.Bucket(FILES_FOR_DOWNLOAD_BUCKET_NAME).objects.all():
-        if s3_object.key.endswith(".pavlovtv"):
-            name = s3_object.key.split("/")[1]
-            timestamp = name.split(" ")[0]
-            entries[timestamp] = f"<li><a href='https://pavlovtv-files-for-download.s3-website.fr-par.scw.cloud//{s3_object.key}'>{name}</a>" \
-                                 f" ({hurry.filesize.size(s3_object.size)}B)</li>"
-    for _, v in sorted(entries.items()):
-        anchor_list = v + anchor_list
-    return HTMLResponse(
-        """
-        <!doctype html>
-        <html>
-        <head>
-        <style>
-            @import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
-            body{
-                font-family: 'Open Sans', sans-serif;
-            }
-        </style>
-        <title>All recordings</title>
-        </head>
-        <body>
-        <ol>
-        """
-        + anchor_list +
-        """
-        </ol>
-        Recordings are retained for 24 hours, so download them before they're gone!
-        <br/>
-        With ❤️ by <a href="//lucy.sh">Lucy</a>
-        </body>
-        </html>
-        """
-    )
-
+    return {"ip": request.client.host}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
---
+++ containers/mitm/app/main.py
@@ -0,0 +1,108 @@
+import json
+import os
+
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse, StreamingResponse, Response
+from httpx import AsyncClient
+from starlette.background import BackgroundTask
+
+PORT = os.environ.get("PORT")
+DATA_DIR = "./data"
+
+allowed_origins = [
+    "http://localhost",
+    "https://tv.cikez.net"
+]
+
+app = FastAPI(
+    title="mitm.tv.cikez.net"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+http_client = AsyncClient(base_url="https://tv.vankrupt.net:443/", verify=False)
+
+def get_all_replays():
+    replays = []
+    for replay_id in os.listdir(DATA_DIR):
+        replay_path = os.path.join(DATA_DIR, replay_id, "metadata.json")
+        if os.path.exists(replay_path):
+            with open(replay_path, "r") as file:
+                replay_data = json.load(file)
+                replays.append(replay_data["find"])
+    return replays
+
+@app.get("/")
+def home():
+    return RedirectResponse("https://tv.cikez.net")
+
+@app.get("/find/any")
+async def list_replays():
+    replays = get_all_replays()
+    return {"replays": replays}
+
+@app.get("/meta/{replay_id}")
+async def meta(replay_id: str):
+    replay_path = os.path.join(DATA_DIR, replay_id, "metadata.json")
+    if os.path.exists(replay_path):
+        with open(replay_path, "r") as file:
+            replay_data = json.load(file)
+            return replay_data["meta"]
+    else:
+        request = http_client.build_request("GET", f"/meta/{replay_id}")
+        response = await http_client.send(request, stream=True)
+        return StreamingResponse(response.aiter_raw(), background=BackgroundTask(response.aclose), headers=response.headers)
+
+@app.get("/replay/{replay_id}/file/{file_name}")
+async def get_replay_file(replay_id: str, file_name: str):
+    file_path = os.path.join(DATA_DIR, replay_id, file_name)
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as file:
+            return Response(content=file.read(), status_code=200)
+    else:
+        request = http_client.build_request("GET", f"/replay/{replay_id}/file/{file_name}")
+        response = await http_client.send(request, stream=True)
+        return StreamingResponse(response.aiter_raw(), background=BackgroundTask(response.aclose), headers=response.headers)
+
+@app.get("/replay/{replay_id}/event")
+async def get_events(replay_id: str):
+    replay_path = os.path.join(DATA_DIR, replay_id, "metadata.json")
+    if os.path.exists(replay_path):
+        with open(replay_path, "r") as file:
+            replay_data = json.load(file)
+            return replay_data["events"]
+    else:
+        request = http_client.build_request("GET", f"/replay/{replay_id}/event")
+        response = await http_client.send(request, stream=True)
+        return StreamingResponse(response.aiter_raw(), background=BackgroundTask(response.aclose), headers=response.headers)
+
+@app.post("/replay/{replay_id}/startDownloading")
+async def start_downloading(replay_id: str, user: str):
+    replay_path = os.path.join(DATA_DIR, replay_id, "metadata.json")
+    if os.path.exists(replay_path):
+        with open(replay_path, "r") as file:
+            replay_data = json.load(file)
+            return replay_data["start_downloading"]
+    else:
+        request = http_client.build_request("POST", f"/replay/{replay_id}/startDownloading?user={user}")
+        response = await http_client.send(request, stream=True)
+        return StreamingResponse(response.aiter_raw(), background=BackgroundTask(response.aclose), headers=response.headers)
+
+@app.post("/replay/{replay_id}/viewer/{viewer_id}")
+def replay_viewer():
+    return Response(content="", status_code=204)
+
+@app.get("/__tv.cikez.net/relay")
+def relay():
+    return {"__tv.cikez.net/relay": True}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8080)
