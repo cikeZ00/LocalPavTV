bcd9b4d410af48aedc30d04a025cdd1caff0f39c
cikeZ00 <17727672+cikeZ00@users.noreply.github.com>
2025-02-11 22:56:19 +0100

    Sort replays by newest when sending to pavtv

--- containers/mitm/app/main.py
+++ containers/mitm/app/main.py
@@ -56,6 +56,7 @@ def get_all_replays():
             with open(replay_path, "r") as file:
                 replay_data = json.load(file)
                 replays.append(replay_data["find"])
+    replays.sort(key=lambda x: x["created"], reverse=True)
     return replays
 
 @app.get("/")
